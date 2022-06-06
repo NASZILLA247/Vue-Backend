5a2671e27ba3d5bca935a162663d16aeb6298f6c
NASZILLA247 <100278085+NASZILLA247@users.noreply.github.com>
2022-06-07 02:10:59 +0400

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 const ObjectID = require('mongodb').ObjectId;
 const MongoClient = require('mongodb').MongoClient;
 let db;
-MongoClient.connect('mongodb+srv://idifavour:Junebuzz123@cluster0.9zqd2.mongodb.net/test', (err, client) => {
+MongoClient.connect('mongodb+srv://nasiru:Loggedin123@cluster0.sb89n.mongodb.net/test', (err, client) => {
     db = client.db('webstore')
 })
 
